1efb74375d6a0f462267a59af0a7912f78f9c97a
karthick10596 <karthick10596@gmail.com>
2023-05-03 12:42:11 +0530

    fibanocci series added

---
+++ src/logicalProgramsdemo/FibonacciSeries.java
@@ -0,0 +1,14 @@
+package logicalProgramsdemo;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//0 1 1 2 3 5 8 13 21 34
+		int n1=0,n2=1,sum=0;
+		System.out.println(n1+" "+n2);
+		
+
+	}
+
+}
