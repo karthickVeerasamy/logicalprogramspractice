f01bdc184887274112a9c97012191b34a8e6aa03
karthick10596 <karthick10596@gmail.com>
2023-05-03 13:12:42 +0530

    Fibanocci updated

--- src/logicalProgramsdemo/FibonacciSeries.java
+++ src/logicalProgramsdemo/FibonacciSeries.java
@@ -1,14 +1,28 @@
 package logicalProgramsdemo;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//0 1 1 2 3 5 8 13 21 34
-		int n1=0,n2=1,sum=0;
-		System.out.println(n1+" "+n2);
-		
+//		Scanner scan =new Scanner(System.in);
+//		System.out.println("Enter the number");
+//		int num=scan.nextInt();
+		for(int i=1;i<15;i++) {
+		System.out.println(getFibanocci(i));
+		}
 
+	}
+	
+	public static int getFibanocci(int num) {
+		if(num==0 || num==1) {
+			return 0;
+		}else if(num==2) {
+            return 1;
+		}
+		return getFibanocci(num-1)+getFibanocci(num-2);
 	}
 
 }
